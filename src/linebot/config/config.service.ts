12c29392f533834ec5965e8553f1efe852419eec
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-23 21:32:02 +0900

    update: setting environment variables

--- src/linebot/config/config.service.ts
+++ src/linebot/config/config.service.ts
@@ -1,4 +1,15 @@
+import { Client } from '@line/bot-sdk';
 import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
 
 @Injectable()
-export class ConfigService {}
+export class ConfigService {
+  createLinebotClient() {
+    const configService = new NestConfigService();
+    const tokens = {
+      channelAccessToken: configService.get<string>('CHANNEL_ACCESS_TOKEN'),
+      channelSecret: configService.get<string>('CHANNEL_SECRET'),
+    };
+    return new Client(tokens);
+  }
+}
