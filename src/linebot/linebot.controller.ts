484dfc9949ce7c86a247e192e5193977fce6701c
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-23 22:13:58 +0900

    update: linebot.controller.ts

--- src/linebot/linebot.controller.ts
+++ src/linebot/linebot.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { WebhookEvent, WebhookRequestBody } from '@line/bot-sdk';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ConfigService as NestConfigService } from './config/config.service';
 
 @Controller('linebot')
-export class LinebotController {}
+export class LinebotController {
+  constructor(private configService: NestConfigService) {}
+
+  @Post()
+  async handler(@Body() req: WebhookRequestBody) {
+    const events: WebhookEvent[] = req.events;
+    events.map((event) => {
+      if (event.type === 'message') {
+        const returnMessage =
+          event.message.type === 'text'
+            ? event.message.text
+            : 'テキストではありませんでした。';
+        return this.configService
+          .createLinebotClient()
+          .replyMessage(event.replyToken, {
+            type: 'text',
+            text: returnMessage,
+          });
+      }
+    });
+  }
+}
